434e043990e64361d0eaec18970ffe39f6d12b10
Kalana_KD <kalanakd471@gmail.com>
2025-08-17 06:09:47 +0530

    Create Review.java

---
+++ src/main/java/com/kd/SalonFlow/entity/Review.java
@@ -0,0 +1,17 @@
+package com.kd.SalonFlow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int reviewId;
+}
