1af065ae49e171a1284d532c21db4ad0db8afbd5
Kalana_KD <kalanakd471@gmail.com>
2025-08-18 11:59:06 +0530

    Create SERVICE Controller,DTO,Repo,Service

---
+++ src/main/java/com/kd/SalonFlow/repo/ServiceRepo.java
@@ -0,0 +1,9 @@
+package com.kd.SalonFlow.repo;
+
+import com.kd.SalonFlow.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepo extends JpaRepository<Service,Integer> {
+}
