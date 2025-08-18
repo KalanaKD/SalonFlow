b1eb94931629fa18d6bae8ca595ffcf972c98789
Kalana_KD <kalanakd471@gmail.com>
2025-08-18 11:57:44 +0530

    Update Main

--- src/main/java/com/kd/SalonFlow/SalonFlowApplication.java
+++ src/main/java/com/kd/SalonFlow/SalonFlowApplication.java
@@ -1,7 +1,9 @@
 package com.kd.SalonFlow;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SalonFlowApplication {
@@ -9,6 +11,11 @@ public class SalonFlowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SalonFlowApplication.class, args);
 		System.out.println("hello database");
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
