e8924902070b7ef57a8b2ca2130728e9945d9cb7
Kalana_KD <kalanakd471@gmail.com>
2025-08-19 03:58:22 +0530

    Update user

--- src/main/java/com/kd/SalonFlow/entity/User.java
+++ src/main/java/com/kd/SalonFlow/entity/User.java
@@ -1,12 +1,11 @@
 package com.kd.SalonFlow.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -22,5 +21,11 @@ public class User {
     private String password;
     private String role;
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
 
 }
